ca7ca3fdd4f50f1dac72a815ab896a23fbd7f9c0
dulgan <antoine.harlin@gmail.com>
2016-08-10 16:55:05 +0200

    Made containerView for AlertView public
    Made containerView for AlertView public  in order to customize it as needed

--- DOAlertController/DOAlertController.swift
+++ DOAlertController/DOAlertController.swift
@@ -153,7 +153,7 @@ public class DOAlertController : UIViewController, UITextFieldDelegate, UIViewCo
     public var overlayColor = UIColor(red:0, green:0, blue:0, alpha:0.5)
     
     // ContainerView
-    private var containerView = UIView()
+    public var containerView = UIView()
     private var containerViewBottomSpaceConstraint: NSLayoutConstraint!
     
     // AlertView
